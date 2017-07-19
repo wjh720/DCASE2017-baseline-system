444d98313f0c6b234fffdd8a61c876c63ceafeb1
hta1996 <huangtaoan@163.com>
2017-07-19 23:08:24 +0800

    drop

--- dcase_framework/keras_utils.py
+++ dcase_framework/keras_utils.py
@@ -421,7 +421,7 @@ class KerasMixin(object):
         vector_label_k = Reshape((dim_vector, ))(vector_label_k_1)
 
         ### Dense
-        Dense_feature_1 = Dense(dim_vector,activation='relu')#, kernel_constraint = max_norm(max_value=2, axis=0))
+        Dense_feature_1 = Dropout(0.2)(Dense(dim_vector,activation='relu'))#, kernel_constraint = max_norm(max_value=2, axis=0))
         vector_feature_i_1 = Dense_feature_1(input_feature)
         vector_feature_k_1 = Dense_feature_1(k_feature)
 
