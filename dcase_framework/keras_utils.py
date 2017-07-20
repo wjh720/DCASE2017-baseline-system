c60c26337ebc27e61901beee5d5f0ac6aa218e4b
wjh720 <wjh720.eric@gmail.com>
2017-07-20 12:16:13 +0800

    Sad

--- dcase_framework/keras_utils.py
+++ dcase_framework/keras_utils.py
@@ -398,8 +398,9 @@ class KerasMixin(object):
         def Loss1(y_true, y_pred):
             l_i, l_k, f_i, f_k = y_pred[:, a * 0 : a], y_pred[:, a : a * 2], \
                                 y_pred[:, a * 2 : a * 3], y_pred[:, a * 3 : a * 4]
-            return (hinge(l_i, f_i, f_k) + hinge(f_i, l_i, l_k)) / NP(2)
+            #return (hinge(l_i, f_i, f_k) + hinge(f_i, l_i, l_k)) / NP(2)
             #return Cos_is(l_i, f_i) - Cos_is(l_i, f_k) - Cos_is(l_k, f_i)
+            return np.array(0)
 
         def Norm(X):
             return K.transpose(K.transpose(X) / (K.sqrt(tf.reduce_sum(K.square(X), 1) + 1e-9)))
@@ -407,7 +408,8 @@ class KerasMixin(object):
         def shit_ik(X):
             l_i, l_k, f_i, f_k = Norm(X[:, a * 0 : a]), Norm(X[:, a : a * 2]), \
                                 Norm(X[:, a * 2 : a * 3]), Norm(X[:, a * 3 : a * 4])
-            return tf.concat([l_i, l_k, f_i, f_k], 1)
+            #return tf.concat([l_i, l_k, f_i, f_k], 1)
+            return X
 
         print(" Begin ! ")
 
