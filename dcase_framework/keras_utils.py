9aa2cac1b209e620748278c732fa217d95b80716
wjh720 <wjh720.eric@gmail.com>
2017-07-21 02:54:41 +0800

    Revert "Staff"
    This reverts commit 1bf0b8693de3d3eb05c7ad799a92ff79d3351b34.

--- dcase_framework/keras_utils.py
+++ dcase_framework/keras_utils.py
@@ -291,10 +291,10 @@ class KerasMixin(object):
             print(y.shape)
             ldata=[]
             for i in range(499):
-                ldata.append(y[i*sr/50:(i+2)*sr/50])
+                ldata.append(y[i*sr/25:(i+2)*sr/25])
             tp=numpy.array(ldata)
             print(tp.shape)
-            
+            #tp=tp.reshape(tp.shape[0],1)
             x=data[item].feat[0]
             tp=numpy.concatenate([tp,x[1:500]], axis = 1)
             pdata.append(tp)
