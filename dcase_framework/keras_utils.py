da4148dba2901403f6d354d6cf1a2dde31f16abc
wjh720 <wjh720.eric@gmail.com>
2017-07-20 12:12:42 +0800

    Def

--- dcase_framework/keras_utils.py
+++ dcase_framework/keras_utils.py
@@ -398,7 +398,7 @@ class KerasMixin(object):
         def Loss1(y_true, y_pred):
             l_i, l_k, f_i, f_k = y_pred[:, a * 0 : a], y_pred[:, a : a * 2], \
                                 y_pred[:, a * 2 : a * 3], y_pred[:, a * 3 : a * 4]
-            return (hinge(l_i, f_i, f_k) + hinge(f_i, l_i, l_k)) / 2
+            return (hinge(l_i, f_i, f_k) + hinge(f_i, l_i, l_k)) / NP(2)
             #return Cos_is(l_i, f_i) - Cos_is(l_i, f_k) - Cos_is(l_k, f_i)
 
         def Norm(X):
