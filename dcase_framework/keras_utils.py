bac0f967c03d4fb5bb6e03e966ffcde3deb2ad4e
hta1996 <huangtaoan@163.com>
2017-07-21 15:05:09 +0800

    np

--- dcase_framework/keras_utils.py
+++ dcase_framework/keras_utils.py
@@ -569,7 +569,7 @@ class KerasMixin(object):
         LSTM_1 = LSTM(units = dim_vector,return_sequences=False)
         #vector_feature_lstm_3=LSTM_1(input_feature)
         Flatten_1=Flatten()
-        raw_feature_tmp = Reshape((84,word_num/84))(raw_feature)
+        raw_feature_tmp = Reshape((200,1))(input_feature)
         vector_feature_lstm_1 = LSTM_1(raw_feature_tmp)
         '''
         ### Dense
