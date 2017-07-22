eadba455206eb3636123688d5b52934aa3c9efd9
hta1996 <huangtaoan@163.com>
2017-07-22 21:08:00 +0800

    soft

--- dcase_framework/keras_utils.py
+++ dcase_framework/keras_utils.py
@@ -724,8 +724,10 @@ class KerasMixin(object):
         Flatten_input = Flatten()(pool_4_input)
         Conv_9 = Conv2D(15, (1, 1))
         conv_9_input = Conv_8(pool_4_input)
-        Soft = Activation(K.softmax(axis=1))
-        Soft_1_input = Soft(conv_9_input)
+        S1 = Permute((2,3,1))(conv_9_input)
+        Soft = Activation('softmax')
+        Soft_1_input = Soft(S1)
+        S2 = Permute((3,1,2))(conv_9_input)
 
         Lam = Lambda(func, output_shape = (15, ), name = 'out_1')(Soft_1_input)
         Dense_1 = Dense(15, activation='softmax', name = 'out_1')
