e00c45c2935f4314a0d6ecd8837e412eb3b3595e
hta1996 <huangtaoan@163.com>
2017-07-22 20:39:48 +0800

    sof

--- dcase_framework/keras_utils.py
+++ dcase_framework/keras_utils.py
@@ -724,7 +724,7 @@ class KerasMixin(object):
         Flatten_input = Flatten()(pool_4_input)
         Conv_9 = Conv2D(15, (1, 1))
         conv_9_input = Conv_8(pool_4_input)
-        Soft = Softmax(axis=1)(conv_9_input)
+        Soft = softmax(axis=1)(conv_9_input)
 
         Lam = Lambda(func, output_shape = (15, ), name = 'out_1')(Soft)
         Dense_1 = Dense(15, activation='softmax', name = 'out_1')
