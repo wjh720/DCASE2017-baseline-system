a7f8df0978dd6542de79469c5a7ca0c0f596790e
hta1996 <huangtaoan@163.com>
2017-07-23 14:56:25 +0800

    np

--- dcase_framework/keras_utils.py
+++ dcase_framework/keras_utils.py
@@ -394,7 +394,7 @@ class KerasMixin(object):
         pdata=[]
         for item in files:
             ve = activity_matrix_dict[item]
-            pdata.append(ve[0].reshape(1, 15).repeat(501, axis = 0))
+            pdata.append(ve[0].reshape(1, 15).repeat(1285, axis = 0))
             #pdata.append(ve)
 
         pdata = np.array(pdata)
@@ -664,7 +664,7 @@ class KerasMixin(object):
         dense_size = 128
         input_size = 501
         raw_size = 441001
-        num_asd = 501
+        num_asd = 1285
 
         def my_loss(y_true, y_pred):
             '''
